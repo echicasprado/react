0a74418e8df323064a5424bc664be13b6a2b50ac
Ever <everchicasp@outlook.com>
2021-12-25 02:02:22 -0600

    Contador

---
+++ 02-Contador/components/Contador.js
@@ -0,0 +1,11 @@
+const Contador = () => {
+    return (
+        <div>
+            <h1>Contador: </h1>
+            <hr />
+            <button onClick="">Aumentar</button>
+            <button>Disminuir</button>
+        </div>
+        
+    );
+}
